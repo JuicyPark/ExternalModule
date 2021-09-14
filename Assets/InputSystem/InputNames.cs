c944527ea51897672b5c59705f507cf90e087cbe
JuicyPark <bassoff@naver.com>
2021-09-14 22:33:47 +0900

    feat : InputSystem 모듈 추가

---
+++ Assets/InputSystem/InputNames.cs
@@ -0,0 +1,21 @@
+// Input System에 의해서 자동으로 생성되는 Enum입니다.
+namespace InputSystem
+{
+    public static class InputSystemSetting
+    {
+        public const string Path = "InputSystem/InputSetting";
+    }
+
+    public enum KeyName
+    {
+        Left,
+        Right,
+        Up,
+        Down,
+        Crouch,
+        Walk,
+        Jump,
+        Sprint,
+        Attack,
+    }
+}
